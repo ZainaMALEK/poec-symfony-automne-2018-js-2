c140f93f05aab0e55e25e2931b9b8ac1735b54c2
Vincent Letourneau <vincent@nanoninja.com>
2018-10-19 08:50:24 +0200

    J4 end

--- starwars/js/app.js
+++ starwars/js/app.js
@@ -3,6 +3,7 @@
   $(document).ready(function() {
     const game$ = $('#game');
     const ship$ = $('#ship');
+    let asteroids$ = $('.asteroid');
 
     animBg();
     generateAsteroid();
@@ -22,14 +23,43 @@
 
     function animBg() {
       setInterval(() => {
+
+        // déplace l'arrière-plan vers le bas
         game$.css('background-position-y', '+=5');
-      }, 1000 / 24)
+
+        // déplacement des astéroides
+        asteroids$.css('top', '+=5');
+
+        asteroids$.each(function() {
+          let aste = $(this);
+          let asteTop = aste.offset().top;
+
+          if (asteTop > 390) {
+            // zone à risque, collission possible avec le vaisseau
+            // récupération de la position x du vaisseau et de l'astéroide
+            let shipLeft = ship$.offset().left;
+            let asteLeft = aste.offset().left;
+
+            if ((asteLeft + 40 >= shipLeft)
+              && (asteLeft <= shipLeft + 50)) {
+                ship$.remove();
+              }
+          }
+
+          if(asteTop > 470) {
+            aste.remove();
+          }
+        })
+
+      }, 1000/24)
     }
 
     function generateAsteroid() {
       setInterval(() => {
+
         let style = 'left:' + getRandomValue() + 'px';
         game$.append('<div class="asteroid" style="'+style+'"></div>');
+        asteroids$ = $('.asteroid');
       }, 3 * 1000)
     }
 
